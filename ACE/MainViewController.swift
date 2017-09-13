5a733c4e19ee5d8b9494f3fa1c51ace66c44b262
Frederick C. Lee <frederick.c.lee@gmail.com>
2017-09-13 11:04:02 -0700

    Squashed commit of the following:
    commit afd68879d148f141dfb3f761dfd646a49fdb292b
    Author: Frederick C. Lee <frederick.c.lee@gmail.com>
    Date:   Wed Sep 13 11:01:38 2017 -0700
    
        Add Swiftlint
    
    commit a1c16de090b3816390ee317fe8a8d949c96efc12
    Author: Frederick C. Lee <frederick.c.lee@gmail.com>
    Date:   Tue Sep 12 16:53:27 2017 -0700
    
        The Skeleton.

--- ACE/MainViewController.swift
+++ ACE/MainViewController.swift
@@ -1,10 +1,15 @@
-//
 //  MainViewController.swift
 //  ACE
 //
+//  Reference:
+//      https://github.com/UncleRic/Ace
+//      https://ace.c9.io/#nav=embedding
+//      https://www.youtube.com/watch?v=NGBTRySd1Ys&t=344s
+//      https://www.youtube.com/watch?v=9OYlTa7YP0Y&t=8s
+//
 //  Created by Frederick C. Lee on 9/12/17.
 //  Copyright © 2017 Amourine Technologies. All rights reserved.
-//
+// -----------------------------------------------------------------------------------------------------
 
 import UIKit
 import WebKit
@@ -18,10 +23,10 @@ class MainViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+    // ... CODE
     }
+    // -----------------------------------------------------------------------------------------------------
 
     @IBAction func exitAction(_ sender: Any) {
         exit(0)
